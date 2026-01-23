f0cf27f9a35e614a385a8969b0c46846dd13fd76
sesa58149 <modi.p158@gmail.com>
2026-01-23 13:44:28 -0500

    added testAuto

--- FTC_TeamCode/source/Constants.java
+++ FTC_TeamCode/source/Constants.java
@@ -11,57 +11,47 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
-
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(11.88)
-            .forwardZeroPowerAcceleration(-40.2)
-            .lateralZeroPowerAcceleration(-61)
+            .mass(30)
+            .forwardZeroPowerAcceleration(-35.837)
+            .lateralZeroPowerAcceleration(-95.29854)
             .translationalPIDFCoefficients(new PIDFCoefficients(0.15,0,0.01,0.02))
             .headingPIDFCoefficients(new PIDFCoefficients(1.1,0,0.015,0.025))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.7,0.0,0.00008,0.82,0.025))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.7,0,0.00008,0.82, 0.025))
             .centripetalScaling(0.0005);
-
-
-
-    ;
-
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(0.2)
+            .maxPower(1)
             .rightFrontMotorName("FR")
             .rightRearMotorName("BR")
-            .leftRearMotorName("BL")
             .leftFrontMotorName("FL")
+            .leftRearMotorName("BL")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(59.76584222748524)
-            .yVelocity(50.39463426)
-            ;
+            .xVelocity(10.574555)
+            .yVelocity(17.11);
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99,
-            100,
-            2.35,
-            1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(-6.2)
-            .strafePodX(-6.5)
+            .forwardPodY(-8)
+            .strafePodX(-9)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
-            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pinpointLocalizer(localizerConstants)
-                .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
-                .pinpointLocalizer(localizerConstants)   // <-- FIX ADDED
+                .pathConstraints(pathConstraints)
                 .build();
     }
 }
---
+++ FTC_TeamCode/source/testAuto.java
@@ -0,0 +1,95 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@Autonomous(name = "Test Auto", group = "Examples")
+public class testAuto extends LinearOpMode{
+    public PathChain Path1;
+    public PathChain Path2;
+    public PathChain Path3;
+    private Follower follower;
+
+    void buildPath(){
+
+        Path1 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 8.000),
+
+                                new Pose(56.000, 35.500)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(0))
+
+                .build();
+
+        Path2 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(56.000, 35.500),
+
+                                //new Pose(23.000, 35.500)
+                                new Pose(30, 35.500)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(0))
+
+                .build();
+
+        Path3 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                //new Pose(23.000, 35.500),
+                                new Pose(30.000, 35.500),
+
+                                new Pose(56.000, 8.000)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
+
+                .build();
+    }
+    void updateTele(){
+        telemetry.addData("XPos =%d" ,follower.getPose().getX());
+        telemetry.addData("Ypos= %d ", follower.getPose().getY());
+        telemetry.addData("heading = %d", follower.getHeading());
+        telemetry.update();
+
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        follower = Constants.createFollower(hardwareMap);
+        buildPath();
+
+        follower.setStartingPose(  new Pose(56.000, 8.000,Math.toRadians(90)));
+        updateTele();
+        waitForStart();
+        follower.followPath(Path1);
+        follower.update();
+
+        while (follower.isBusy())
+        {
+            follower.update();
+            updateTele();
+        }
+        updateTele();
+        sleep(2000);
+         follower.followPath(Path2);
+         follower.update();
+        while (follower.isBusy())
+        {
+          follower.update();
+          updateTele();
+        }
+        sleep(2000);
+        follower.followPath(Path3);
+        follower.update();
+        while (follower.isBusy())
+        {
+            follower.update();
+            updateTele();
+        }
+        sleep(2000);
+    }
+
+
+}
